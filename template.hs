8d06d9d58bd7d21f27237652d8cc8bd590a2fc3d
Francisco da Ana <franciscopdana@gmail.com>
2023-12-28 19:24:53 +0000

    first implementation of compiler

--- template.hs
+++ template.hs
@@ -1,3 +1,6 @@
+import Data.List
+import Data.List
+
 -- PFL 2023/24 - Haskell practical assignment quickstart
 -- Updated on 27/12/2023
 
@@ -10,20 +13,61 @@ data Inst =
   deriving Show
 type Code = [Inst]
 
--- createEmptyStack :: Stack
-createEmptyStack = undefined -- TODO, Uncomment the function signature after defining Stack
+data StackElement = I Integer | Tt | Ff
+  deriving Show
+type Stack = [StackElement]
 
--- stack2Str :: Stack -> String
-stack2Str = undefined -- TODO, Uncomment all the other function type declarations as you implement them
+stackElem2Str :: StackElement -> String
+stackElem2Str (I n) = show n
+stackElem2Str Tt = "True"
+stackElem2Str Ff = "False"
 
--- createEmptyState :: State
-createEmptyState = undefined -- TODO, Uncomment the function signature after defining State
+type State = [(String, StackElement)]
 
--- state2Str :: State -> String
-state2Str = undefined -- TODO
+createEmptyStack :: Stack
+createEmptyStack = []
 
--- run :: (Code, Stack, State) -> (Code, Stack, State)
-run = undefined -- TODO
+
+stack2Str :: Stack -> String
+stack2Str stack = intercalate "," (map stackElem2Str stack)
+
+createEmptyState :: State
+createEmptyState = []
+
+state2Str :: State -> String
+state2Str state = intercalate "," (map (\(var, val) -> var ++ "=" ++ stackElem2Str val) sortState)
+  where sortState = sortOn fst state
+
+run :: (Code, Stack, State) -> (Code, Stack, State)
+run ([], stack, state) = ([], stack, state)
+run ((Push n):code, stack, state) = run(code, (I n):stack, state)
+run (Add:code, (I n1):(I n2):stack, state) = run(code, (I (n1 + n2)):stack, state)
+run (Mult:code, (I n1):(I n2):stack, state) = run(code, (I (n1 * n2)):stack, state)
+run (Sub:code, (I n1):(I n2):stack, state) = run(code, (I (n1 - n2)):stack, state)
+run (Tru:code, stack, state) = run(code, Tt:stack, state)
+run (Fals:code, stack, state) = run(code, Ff:stack, state)
+run (Equ:code, (I n1):(I n2):stack, state) = run(code, if n1 == n2 then Tt:stack else Ff:stack, state)
+run (Equ:code, Tt:Tt:stack, state) = run(code, Tt:stack, state)
+run (Equ:code, Tt:Ff:stack, state) = run(code, Ff:stack, state)
+run (Equ:code, Ff:Tt:stack, state) = run(code, Ff:stack, state)
+run (Equ:code, Ff:Ff:stack, state) = run(code, Tt:stack, state)
+run (Le:code, (I n1):(I n2):stack, state) = run(code, if n1 <= n2 then Tt:stack else Ff:stack, state)
+run (And:code, Tt:Tt:stack, state) = run(code, Tt:stack, state)
+run (And:code, Tt:Ff:stack, state) = run(code, Ff:stack, state)
+run (And:code, Ff:Tt:stack, state) = run(code, Ff:stack, state)
+run (And:code, Ff:Ff:stack, state) = run(code, Ff:stack, state)
+run (Neg:code, Tt:stack, state) = run(code, Ff:stack, state)
+run (Neg:code, Ff:stack, state) = run(code, Tt:stack, state)
+run (Fetch var:code, stack, state) = run(code, (val):stack, state)
+  where Just val = lookup var state
+run (Store var:code, val:stack, state) = run(code, stack, (var, val):state)
+run (Noop:code, stack, state) = run(code, stack, state)
+run (Branch code1 code2:code, Tt:stack, state) = run(code, stack, state)
+run (Branch code1 code2:code, Ff:stack, state) = run(code, stack, state)
+run (Branch code1 code2:code, stack, state) = run(code, stack, state)
+run (Loop code1 code2:code, Tt:stack, state) = run(code, stack, state)
+run (Loop code1 code2:code, Ff:stack, state) = run(code, stack, state)
+run (Loop code1 code2:code, stack, state) = run(code, stack, state)
 
 -- To help you test your assembler
 testAssembler :: Code -> (String, String)
@@ -38,7 +82,7 @@ testAssembler code = (stack2Str stack, state2Str state)
 -- testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
 -- testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
 -- testAssembler [Push (-20),Push (-21), Le] == ("True","")
--- testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
+-- MAL testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
 -- testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
 -- If you test:
 -- testAssembler [Push 1,Push 2,And]
