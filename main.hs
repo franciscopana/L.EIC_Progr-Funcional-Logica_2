f7390e6dcbbe2ce60c8b352cfa45e9fcd2867ef4
Francisco da Ana <franciscopdana@gmail.com>
2023-12-31 18:06:52 +0000

    authomatic tests

--- main.hs
+++ main.hs
@@ -401,3 +401,26 @@ testParser programCode = (stack2Str stack, state2Str state)
 -- testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
 -- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
 -- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
+
+runTests :: IO ()
+runTests = mapM_ runTest testCases
+  where
+    runTest (input, expected) = do
+      let result = testParser input
+      if result == expected
+        then putStrLn $ "Passed: " ++ input
+        else putStrLn $ "Failed: " ++ input ++ " expected " ++ show expected ++ " but got " ++ show result
+    testCases = [
+        ("x := 5; x := x - 1;", ("","x=4")),
+        ("x := 0 - 2;", ("","x=-2")),
+        ("if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;", ("","y=2")),
+        ("x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);", ("","x=1")),
+        ("x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;", ("","x=2")),
+        ("x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;", ("","x=2,z=4")),
+        ("x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;", ("","x=34,y=68")),
+        ("x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;", ("","x=34")),
+        ("if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;", ("","x=1")),
+        ("if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;", ("","x=2")),
+        ("x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);", ("","x=2,y=-10,z=6")),
+        ("i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);", ("","fact=3628800,i=1"))
+      ]
